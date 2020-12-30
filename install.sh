69971d5935c42b45104728b2f5074aecb3c3d988
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-30 12:40:58 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo [*]This Script will install the required dependencies for bullet!
+sudo apt install python3-pip
+sudo apt install python3
+sudo pip3 install termcolor
+sudo pip3 install bcrypt
+echo [*]All Dependencies have been successfullly installed if no erros!--!
+echo [#]You can now run Bullet
+
